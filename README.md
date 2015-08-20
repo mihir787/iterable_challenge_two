af65167ab3f0d99fd4e1230bbccafc2b7eb03805
Mihir Parikh <mihir787@gmail.com>
2015-08-20 08:44:07 -0600

    added default route formatting

--- README.md
+++ README.md
@@ -1,6 +1,17 @@
 #README: Challenge Two, Transaction API
 
 ##Deployed API
+https://transactionapi.herokuapp.com/
+
+Example API Call:
+
+transaction_params = {
+  "companyName" => "EAT24",
+  "totalMonthlyActiveUsers" => 10000,
+  "pricingBuckets"=> [ { numUsers: 0, price: 20}, { numUsers: 1000, price: 10} ]
+}
+
+post 'https://transactionapi.herokuapp.com/api/v1/transactions.json', transaction_params
 
 ##How to get running locally?
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,16 @@
 Rails.application.routes.draw do
 
-  namespace :api do
-    namespace :v1 do
-      resources :transactions, only: [:create, :show]
+  # namespace :api do
+  #   namespace :v1 do
+  #     resources :transactions, only: [:create, :show]
+  #   end
+  # end
+
+  scope format: true, defaults: { format: 'json' } do
+    namespace :api do
+      namespace :v1 do
+        resources :transactions, only: [:create, :show]
+      end
     end
   end
 
